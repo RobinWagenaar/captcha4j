aade507bcb0cc65a6f618e79ceb95a0f508cf0d6
Robin <info@robinwagenaar.nl>
2021-11-26 20:28:33 +0100

    Added example files, restructured folder

--- captcha4j-core/src/test/java/com/github/captcha4j/integrationtest/ImageCaptchaTester.java
+++ captcha4j-core/src/test/java/com/github/captcha4j/integrationtest/ImageCaptchaTester.java
@@ -1,23 +1,32 @@
 package com.github.captcha4j.integrationtest;
 
 import com.github.captcha4j.core.image.ImageCaptchaBuilder;
+import com.github.captcha4j.core.image.gimpy.BlockGimpyRenderer;
+import com.github.captcha4j.core.image.gimpy.RippleGimpyRenderer;
+import com.github.captcha4j.core.image.producer.backgrounds.FlatColorBackgroundProducer;
 import com.github.captcha4j.core.image.producer.backgrounds.GradientBackgroundProducer;
+import com.github.captcha4j.core.image.producer.backgrounds.SquigglesBackgroundProducer;
+import com.github.captcha4j.core.image.producer.backgrounds.TransparentBackgroundProducer;
 import com.github.captcha4j.core.image.producer.noise.CurvedLineNoiseProducer;
 import com.github.captcha4j.core.image.ImageCaptcha;
 import com.github.captcha4j.core.image.gimpy.FishEyeGimpyRenderer;
+import com.github.captcha4j.core.image.renderer.ColoredEdgesWordRenderer;
+import com.github.captcha4j.core.image.renderer.DefaultWordRenderer;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
 
 public class ImageCaptchaTester {
 
     public static void main(String... args) throws InterruptedException {
-        ImageCaptcha captcha = new ImageCaptchaBuilder(200, 100)
-                .addRandomAnswer()
-                .addNoise(new CurvedLineNoiseProducer())
-                .addBackground(new GradientBackgroundProducer())
+        ImageCaptcha captcha = new ImageCaptchaBuilder(300, 150)
+                .addBackground(new FlatColorBackgroundProducer(Color.ORANGE))
+                .addText(()->"NiceRipple",
+                        new DefaultWordRenderer(Arrays.asList(Color.RED), Arrays.asList(new Font("cooper black", Font.PLAIN, 50)))
+                )
                 .gimp(new FishEyeGimpyRenderer())
-                .addBorder()
                 .build();
 
         JFrame frame = new JFrame("Image captcha test");
